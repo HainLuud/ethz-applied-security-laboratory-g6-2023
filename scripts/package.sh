1cbf37de3120422218aa89fb62e111da203efa1f
Damiano Amatruda <damatruda@student.ethz.ch>
2023-11-24 20:44:15 +0100

    Global: Remove Vagrant NAT from client machine

--- scripts/package.sh
+++ scripts/package.sh
@@ -8,10 +8,18 @@ package_dir="$1"
 
 mkdir -p "$package_dir"
 
-vagrant up
+vagrant halt
 
+# Remove the Vagrant network interface from the client machine
+VBoxManage modifyvm client --nic1 none
+
+# Export each machine to an OVA file in parallel
 for machine_name in "${machine_names[@]}"; do
     VBoxManage export "$machine_name" -o "$package_dir/$machine_name.ova" &
 done
 
+# Wait for all exports to finish
 wait
+
+# Re-add the Vagrant network interface to the client machine
+VBoxManage modifyvm client --nic1 nat
