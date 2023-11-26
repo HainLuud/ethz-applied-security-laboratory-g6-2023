353853b36c50b619366e8442f9b2f13262fb6d5f
Damiano Amatruda <damatruda@student.ethz.ch>
2023-11-26 15:49:48 +0100

    OBFUSCATOR: Use own UID and GID in Docker
    Fixes error: [Errno 13] Permission denied: '/app/obfuscate/ca/server/ca.c'.
    
    Co-authored-by: HainLuud <HainLuud@users.noreply.github.com>
    Co-authored-by: jabbadizzleCode <jabbadizzleCode@users.noreply.github.com>

--- scripts/obfuscate-cythonize.sh
+++ scripts/obfuscate-cythonize.sh
@@ -9,6 +9,6 @@ dst="$2"
 
 cp -rf "$src/"* "$dst/"
 mv "$dst/gunicorn.conf.py" "$dst/gunicorn_conf.py"
-docker compose -f obfuscator/compose.yaml run --build --rm --volume "$(pwd):/app" cythonize -3 -i --no-docstrings "$dst/"*.py
+docker compose -f obfuscator/compose.yaml run --build --rm --user "$(id -u):$(id -g)" --volume "$(pwd):/app" cythonize -3 -i --no-docstrings "$dst/"*.py
 rm -rf "$dst/build" "$dst/"*.py "$dst/"*.c
 echo 'from gunicorn_conf import *' > "$dst/gunicorn.conf.py"
--- scripts/obfuscate-pyarmor.sh
+++ scripts/obfuscate-pyarmor.sh
@@ -5,5 +5,5 @@ set -e
 src="$1"
 dst="$2"
 
-docker compose -f obfuscator/compose.yaml run --build --rm --volume "$(pwd):/app" pyarmor gen -O "$dst" "$src/"*.py
+docker compose -f obfuscator/compose.yaml run --build --rm --user "$(id -u):$(id -g)" --volume "$(pwd):/app" pyarmor gen -O "$dst" "$src/"*.py
 sed -i '' '/^# Pyarmor/d' "$dst/"*.py
